7e83cb96398740f030a8ef81b1a8be8aff5bb09c
NekronFeonix <myeo0008@student.monash.edu>
2023-04-30 02:13:24 +0800

    30/4/2023 2:13am Update tick function that can spawn lone wolf every turn.

--- src/game/environment/GustOfWind.java
+++ src/game/environment/GustOfWind.java
@@ -1,11 +1,25 @@
 package game.environment;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.RandomNumberGenerator;
+import game.enemy.LoneWolf;
 
 public class GustOfWind extends Ground {
     
     public GustOfWind() {
 		super('&');
+	}
+
+	@Override
+	public void tick(Location location) {
+		super.tick(location) ;
+
+			int p = RandomNumberGenerator.getRandomInt(100) ;
+			
+			if (p < 33 && !location.map().isAnActorAt(location)) {
+				location.map().at(location.x(), location.y()).addActor(new LoneWolf())  ;
+			}
 	}
 }
 
