7c6860a8dad0b6ffb5ea23038b130d4589904fb8
NekronFeonix <myeo0008@student.monash.edu>
2023-04-30 01:56:49 +0800

    30/4/2023 1:55am Update tick function that can spawn skeleton every turn

--- src/game/environment/Graveyard.java
+++ src/game/environment/Graveyard.java
@@ -1,10 +1,28 @@
 package game.environment;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import game.RandomNumberGenerator;
+import edu.monash.fit2099.engine.positions.Location;
+import game.enemy.HeavySkeletonSwordsman;
+import game.enemy.LoneWolf;
 
 public class Graveyard extends Ground {
 	
     public Graveyard() {
 		super('n');
 	}
+
+	@Override
+	public void tick(Location location) {
+		super.tick(location) ;
+
+			int p = RandomNumberGenerator.getRandomInt(100) ;
+			
+			if (p < 27 && !location.map().isAnActorAt(location)) {
+				location.map().at(location.x(), location.y()).addActor(new HeavySkeletonSwordsman())  ;
+			}
+				 
+	}
+
+	
 }
