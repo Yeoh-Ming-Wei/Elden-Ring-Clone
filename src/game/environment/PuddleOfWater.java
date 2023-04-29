4ea4f41ba017722851f4de21e041a3f7c885a42c
NekronFeonix <myeo0008@student.monash.edu>
2023-04-30 02:15:32 +0800

    30/4/2023 2:15am Update tick function that can spawn giant crab every turn.

--- src/game/environment/PuddleOfWater.java
+++ src/game/environment/PuddleOfWater.java
@@ -1,10 +1,24 @@
 package game.environment;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.RandomNumberGenerator;
+import game.enemy.GiantCrab;
+
 
 public class PuddleOfWater extends Ground {
     
     public PuddleOfWater() {
         super('~') ;
     }
+
+    public void tick(Location location) {
+		super.tick(location) ;
+
+			int p = RandomNumberGenerator.getRandomInt(100) ;
+			
+			if (p < 2 && !location.map().isAnActorAt(location)) {
+				location.map().at(location.x(), location.y()).addActor(new GiantCrab())  ;
+			}
+	}
 }
