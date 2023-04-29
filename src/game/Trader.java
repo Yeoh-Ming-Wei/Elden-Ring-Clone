e1db4c365f2afbf6bf31c53a7d877917dd3abab1
singy <slee0163@student.monash.edu>
2023-04-29 18:02:39 +0800

    29/4/2023 6:02pm done the printing for purchase action

--- src/game/Trader.java
+++ src/game/Trader.java
@@ -24,9 +24,6 @@ public class Trader extends Actor {
     /**
      * Constructor.
      *
-     * @param name        the name of the Actor
-     * @param displayChar the character that will represent the Actor in the display
-     * @param hitPoints   the Actor's starting hit points
      */
     public Trader() {
         super("Merchant Kale", 'K', 999);
