32429c395195489b36f50d6f10706a35f6b499e9
NekronFeonix <myeo0008@student.monash.edu>
2023-05-02 14:46:10 +0800

    2/5/2023 2:p43m Update RuneManager class with close constructor.

--- src/game/RuneManager.java
+++ src/game/RuneManager.java
@@ -2,9 +2,11 @@ package game;
 
 import edu.monash.fit2099.engine.actors.Actor;
 
-public class RuneManager {
+public class RuneManager{
+    
+    private Rune rune ;
+    private static RuneManager rm = null;
 
-    private static int rune ;
     private static final int LONE_WOLF_MIN_RUNE = 55 ;
     private static final int LONE_WOLF_MAX_RUNE = 1470 ;
     
@@ -13,36 +15,39 @@ public class RuneManager {
     
     private static final int HSS_MIN_RUNE = 35 ;
     private static final int HSS_MAX_RUNE = 892 ;
-    
-    
-    public RuneManager() {
-        rune = 0 ;
-    }
 
-    public static int returnRune() {
-        return rune ;
-    }
+    private RuneManager() {
+        rune = new Rune() ;
+    } ;
 
-    public static void addRune(Actor player, char enemy) {
+    public static RuneManager getInstance() {
+        if (rm == null) 
+            rm = new RuneManager() ;
+        return rm ;
+    }
+    
+    public void addRuneEnemy(Actor player, char enemy) {
         int value = 0 ;
-        System.out.println(enemy) ;
         switch (enemy) {
             case 'h' -> value = RandomNumberGenerator.getRandomInt(LONE_WOLF_MIN_RUNE, LONE_WOLF_MAX_RUNE) ;
             case 'C' -> value = RandomNumberGenerator.getRandomInt(GIANT_CRAB_MIN_RUNE, GIANT_CRAB_MAX_RUNE) ;
             case 'X' -> value = RandomNumberGenerator.getRandomInt(HSS_MIN_RUNE, HSS_MAX_RUNE) ;
         }
-
-        rune += value ;
+        rune.setRune(rune.getRune() + value) ;
         System.out.println(menuDescription(player, "increased", value)) ;
     }
 
-    public static void deductRune(Actor player, int value) {
-        rune -= value ;
+    public void deductRune(Actor player, int value) {
+        rune.setRune(rune.getRune() - value) ;
         System.out.println(menuDescription(player, "decreased", value)) ;
         
     }
 
-    public static String menuDescription(Actor player, String verb, int value) {
-        return String.format("%s's rune has %s by %d, now %s has %d runes.", player, verb, value, player, rune);
+    public int returnRune() {
+        return rune.getRune() ;
+    }
+
+    public String menuDescription(Actor player, String verb, int value) {
+        return String.format("%s's rune has %s by %d, now %s has %d runes.", player, verb, value, player, rune.getRune());
     }
 }
