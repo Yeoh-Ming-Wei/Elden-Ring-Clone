46f9f98df682955bc16d78f928fa24d1ad60bd3a
NekronFeonix <myeo0008@student.monash.edu>
2023-05-03 16:52:51 +0800

    3/5/2023 4:50pm Added enemy factory abstract class.

---
+++ src/game/environment/EnemyFactory.java
@@ -0,0 +1,10 @@
+package game.environment;
+
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+
+public abstract class EnemyFactory {
+
+    public void addEnemy(Ground ground, Location location) {} ;
+
+}
