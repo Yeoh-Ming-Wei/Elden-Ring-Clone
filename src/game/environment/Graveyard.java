708ee316e9344642c1fe6a6d073e50cd501199d7
NekronFeonix <myeo0008@student.monash.edu>
2023-05-03 16:53:40 +0800

    3/5/2023 4:53pm Remove add enemy implementation. Now has east and west side.

--- src/game/environment/Graveyard.java
+++ src/game/environment/Graveyard.java
@@ -1,10 +1,7 @@
 package game.environment;
 
 import edu.monash.fit2099.engine.positions.Ground;
-import game.RandomNumberGenerator;
 import edu.monash.fit2099.engine.positions.Location;
-import game.enemy.HeavySkeletonSwordsman;
-import game.enemy.SkeletalBandit;
 
 public class Graveyard extends Ground {
 	
@@ -16,16 +13,13 @@ public class Graveyard extends Ground {
 	public void tick(Location location) {
 		super.tick(location) ;
 
-			int p = RandomNumberGenerator.getRandomInt(100) ;
-			int mid = location.map().getXRange().max() / 2 ;
-			
-			if (p < 27 && !location.map().isAnActorAt(location)) {
-				if (location.x() < mid) {
-					location.map().at(location.x(), location.y()).addActor(new HeavySkeletonSwordsman()) ;
-				} else {
-					location.map().at(location.x(), location.y()).addActor(new SkeletalBandit()) ;
-				}
-			}
+		int mid = location.map().getXRange().max() / 2 ;
+		
+		if (location.x() < mid) {
+			WestMapEnemyFactory.getInstance().addEnemy(this, location) ;
+		} else {
+			EastMapEnemyFactory.getInstance().addEnemy(this, location) ;
+		}
 				 
 	}
 
--- src/game/environment/PuddleOfWater.java
+++ src/game/environment/PuddleOfWater.java
@@ -2,10 +2,6 @@ package game.environment;
 
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
-import game.RandomNumberGenerator;
-import game.enemy.GiantCrab;
-import game.enemy.GiantCrayFish;
-
 
 public class PuddleOfWater extends Ground {
     
@@ -16,15 +12,7 @@ public class PuddleOfWater extends Ground {
     public void tick(Location location) {
 		super.tick(location) ;
 
-			int p = RandomNumberGenerator.getRandomInt(100) ;
-			int mid = location.map().getXRange().max() / 2 ;
-			
-			if (p < 2 && !location.map().isAnActorAt(location)) {
-				if (location.x() < mid) {
-					location.map().at(location.x(), location.y()).addActor(new GiantCrab()) ;
-				} else {
-					location.map().at(location.x(), location.y()).addActor(new GiantCrayFish()) ;
-				}
-			}
+	
+				
 	}
 }
