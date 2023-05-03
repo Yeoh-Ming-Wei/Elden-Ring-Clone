d6ef5d936528ec84231fcf7bdcd4a692daa443db
NekronFeonix <myeo0008@student.monash.edu>
2023-05-03 13:51:39 +0800

    3/5/2023 1:50pm Add lone wolf min max rune and enemydroprune function.

--- src/game/enemy/LoneWolf.java
+++ src/game/enemy/LoneWolf.java
@@ -23,9 +23,15 @@ import java.util.Map;
  * Modified by:
  */
 public class LoneWolf extends Dog {
+
+    private final int LONE_WOLF_MIN_RUNE = 55 ;
+    private final int LONE_WOLF_MAX_RUNE = 1470 ;
+
     public LoneWolf() {
         super("Lone Wolf", 'h', 102);
         this.addCapability(ActorTypes.WOLF);
+
+        RuneManager.addEnemyDropRune(name, LONE_WOLF_MIN_RUNE, LONE_WOLF_MAX_RUNE) ;
     }
 
     @Override
