ea7093d20ed6b5f27cbf05460beee023f856c251
NekronFeonix <myeo0008@student.monash.edu>
2023-05-03 20:59:08 +0800

    3/5/2023 8:59pm Remove unused import.

--- src/game/behaviour/WanderBehaviour.java
+++ src/game/behaviour/WanderBehaviour.java
@@ -8,7 +8,6 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
-import game.behaviour.Behaviour;
 
 /**
  * Created by:
