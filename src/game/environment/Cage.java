57beb6e21bea30922c0be13419ef75ea5397db67
NekronFeonix <myeo0008@student.monash.edu>
2023-05-20 01:23:25 +0800

    20/5/2023 1:24AM Spawning Godrick at Cage.

--- src/game/environment/Cage.java
+++ src/game/environment/Cage.java
@@ -2,6 +2,8 @@ package game.environment;
 
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
+import game.RandomNumberGenerator;
+import game.enemy.Dog;
 
 public class Cage extends Ground {
     /**
@@ -13,4 +15,14 @@ public class Cage extends Ground {
         super('<');
     }
 
+    @Override
+	public void tick(Location location) {
+		super.tick(location) ;
+
+        int p = RandomNumberGenerator.getRandomInt(100) ;
+
+        if (p < 37 && !location.map().isAnActorAt(location)) {
+            location.map().at(location.x(), location.y()).addActor(new Dog()) ;
+        }
+	}
 }
