135fd4d1c4a6ce1eb0f2282b81b4bebd364e0006
NekronFeonix <myeo0008@student.monash.edu>
2023-05-20 01:23:01 +0800

    20/5/2023 1:22AM Spawning dog at barracks

--- src/game/environment/Barrack.java
+++ src/game/environment/Barrack.java
@@ -1,6 +1,9 @@
 package game.environment;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.RandomNumberGenerator;
+import game.enemy.GodrickSoldier;
 
 public class Barrack extends Ground {
     /**
@@ -11,4 +14,15 @@ public class Barrack extends Ground {
     public Barrack() {
         super('B');
     }
+
+    @Override
+	public void tick(Location location) {
+		super.tick(location) ;
+
+        int p = RandomNumberGenerator.getRandomInt(100) ;
+
+        if (p < 45 && !location.map().isAnActorAt(location)) {
+            location.map().at(location.x(), location.y()).addActor(new GodrickSoldier()) ;
+        }
+	}
 }
